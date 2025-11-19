6f6d6dfd0905748d0a45b4c46b3ae2f141e9dd01
Arvin <118655569+ARVIN1006@users.noreply.github.com>
2025-11-19 10:54:18 +0700

    Implement admin news management features and update database configuration

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,9 +6,10 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 // $routes->setAutoRoute(value: false);
-$routes->get('/', to: 'Home::index');
-$routes->get('/about', to: 'Page::about');
-$routes->get('/contact', to: 'Page::contact');
-$routes->get('/faqs', to: 'Page::faqs');
-$routes->get('/news', 'News::index');
-$routes->get('/news/(:any)', 'News::viewNews/$1');
+$routes->group('admin', function($routes){
+	$routes->get('news', 'NewsAdmin::index');
+	$routes->get('news/(:segment)/preview', 'NewsAdmin::preview/$1');
+    $routes->add('news/new', 'NewsAdmin::create');
+	$routes->add('news/(:segment)/edit', 'NewsAdmin::edit/$1');
+	$routes->get('news/(:segment)/delete', 'NewsAdmin::delete/$1');
+});
